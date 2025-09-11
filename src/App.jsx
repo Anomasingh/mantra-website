2128747916adc70295761c73cfe91f844f7e65c7
Anomasingh <anomasingh05@gmail.com>
2025-09-11 16:30:05 +0530

    Fix Vercel build error: Remove unused Supabase dependencies and DetailsPage

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainLayout from './layout/MainLayout';
 import HomePage from './pages/HomePage';
 import MostlySearched from './pages/MostlySearched';
-import DetailsPage from "./pages/DetailsPage";  
 import MantrasList from './pages/MantrasList';
 import MantraDetail from './pages/MantraDetail';
 // import God_Goddess from "./pages/God_Goddess"     // General details view
@@ -15,7 +14,6 @@ function App() {
         <Route path="/" element={<MainLayout />}>
           <Route index element={<HomePage />} />
           <Route path="mostly-searched" element={<MostlySearched />} />
-          <Route path="/details/:id" element={<DetailsPage />} />
           <Route path="/mantras" element={<MantrasList />} />
           <Route path="/mantra/:mantraId" element={<MantraDetail />} />
           {/* <Route path="/gods/:id" element={<God_Goddess/>} /> */}
